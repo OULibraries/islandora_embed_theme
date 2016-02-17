6954dbafa455b98834b74c9c20901209e37193a3
Logan Cox <lmc@ou.edu>
2016-02-17 14:34:29 -0600

    initial check in of theme with with any oulibraries modifications

---
+++ page.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Fork of Default theme implementation to display a single Drupal page.
+ *
+ */
+?>
+  <div id="content" class="column"><div class="section">
+     <a id="main-content"></a>
+
+     <?php print render($page['content']); ?>
+  </div></div> <!-- /.section, /#content -->
